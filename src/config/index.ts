9bf14ded4d7881a8dc75478cf50c7716d5923842
Stefania <stefania.dibacco@gmail.com>
2021-11-08 14:21:25 -0300

    31entregable

--- src/config/index.ts
+++ src/config/index.ts
@@ -32,7 +32,7 @@ const flags = args.parse(process.argv);
 
 const env = {
   MONGO_ATLAS_URL: process.env.MONGO || 'mongoSRV',
-  PORT: process.env.PORT || flags.port || 8080,
+  PORT: process.env.PORT || flags.port,
   FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || flags.faceId || 'faceId',
   FACEBOOK_APP_SECRET:
     process.env.FACEBOOK_APP_SECRET || flags.faceSecret || 'faceSecret',
